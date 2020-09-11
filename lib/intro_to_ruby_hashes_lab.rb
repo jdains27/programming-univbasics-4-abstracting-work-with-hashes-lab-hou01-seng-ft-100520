571159fe0414da327e9a09e96bab487a9d44f464
Jordan Dains <dains.jordan2@gmail.com>
2020-09-11 14:12:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash = {location: 'Houston, TX', occupation: 'Student'}
+  hash = [key]
 end
 
 def update_counting_hash(hash, key)
